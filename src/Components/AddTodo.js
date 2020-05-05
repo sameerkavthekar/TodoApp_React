e2bbc6767be95597ea4d677d72f16d7a14cbc67a
Sameer Kavthekar <--global>
2020-05-06 00:57:23 +0530

    Added a Todo React App

---
+++ src/Components/AddTodo.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import Todos from "./Todos";
+
+class AddTodo extends Component {
+    state = {
+        title: ''
+    }
+
+    addButtonHandler = (e) => this.setState({[e.target.name]: e.target.value});
+
+    submitForm = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title: ''});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.submitForm} autoComplete='off' style={{display: 'flex'}}>
+                <input
+                    type="text"
+                    name="title"
+                    style={{flex: '10', padding: '5px'}}
+                    placeholder="Add Todo..."
+                    value={this.state.title}
+                    onChange={this.addButtonHandler}
+                />
+                <input
+                    type="submit"
+                    value="Submit"
+                    className="btn"
+                    style={{flex: '1'}}
+                />
+            </form>
+        );
+    }
+}
+
+AddTodo.propTypes = {
+    addTodo: PropTypes.func.isRequired
+}
+
+
+export default AddTodo;
---
+++ src/Components/Pages/About.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList app v1.0.0. it is a part of a React crash course</p>
+        </React.Fragment>
+    );
+}
+
+export default About;
